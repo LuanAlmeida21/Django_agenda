25b7bd43417128c063e0a6b963daa2a28006555b
Luan ALmeida <luan.ab2002@yahoo.com.br>
2022-12-28 14:49:42 -0300

    Incluido função e tela de autenticação

--- core/views.py
+++ core/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 # Create your views here.
 
-def titulo_evento(request, titulo_evento):
-    local = Evento.objects.get(titulo= titulo_evento)
-    return HttpResponse(f'<h1>{local}</h1>')
+def login_user(request):
+    return render(request, 'login.html')
 
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, 'Usuário ou senha inválido')
+    
+    return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.all()
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
-#def index(request):
-#    return redirect('/agenda/')
-
-
-# eventos.objects.all() - pega todos objetos
-# filter(usuario=usuario) - usuario logado
 
